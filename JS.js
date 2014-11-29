ac687ae51f86a37d75c5743afaac15fdbcc2472c
WorldTreeCircle <worldtreecircle@gmail.com>
2014-11-28 23:34:53 -0500

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,4 +1,10 @@
- //Setup event handlers
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ex 7 - Javascript - Hello World Again</title>
+    <script type="text/javascript">
+        
+        //Setup event handlers
         window.addEventListener("load", setupEventHandlers, false);
 
         //-------------------------------
@@ -30,7 +36,7 @@
         {
             console.log("Inside displayHelloWorld function");
 
-            document.getElementById('divHelloWorld').innerHTML = "<h1>Hello World!</h1>";
+            document.getElementById('divDisplay1').innerHTML = "<h1>Hello World!</h1>";
         }
 
        
@@ -42,3 +48,16 @@
 
             document.getElementById('divDisplay2').innerHTML = "<h2>Hello World Again!</h2>";
         }
+
+    </script>
+</head>
+<body>
+    <h1><i>Hello World Again!</i></h1>
+
+    <input type="button" id="butHelloWorld" value="Show Hello World" />
+    <input type="button" id="butHelloWorldAgain" value="Show Hello World Again" />
+    <br />
+    <div id="divDisplay1"></div>
+    <div id="divDisplay2"></div>
+</body>
+</html>
