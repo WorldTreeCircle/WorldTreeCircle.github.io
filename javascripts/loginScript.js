413fc199b8bf13353f53865718482532c4861a15
Heather and Clarke Ball, Founders <info@worldtreecircle.com>
2015-02-24 22:25:43 -0500

    "Login" page.
    Started working on "Login" page.

---
+++ javascripts/loginScript.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    //Create table with login textbox
+    var content = $("<table id=tblLogin><tr><td><input id='txtEmail' type='text' value='Email' /></td></tr></table>").addClass('loginTxt');
+    //Move imgLogo from center of page to top
+    $('#imgLogo').animate({ top: '25%' }, "slow", function () {
+        $('#imgLogo').after(content); //insert hidden login textbox
+        $('#tblLogin').fadeIn("slow"); //fade login textbox into view
+    });
+
+    //Clear default text on textbox focus
+    $('input').focus(function () {
+        console.log("txtEmail has focus.");
+        $('#txtEmail').val('');
+    })
+});
