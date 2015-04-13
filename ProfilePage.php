8ac127ae1d4da1ce5090075cfb2bad000b93f1a4
Caleb Edwards <edwardscaleb9@gmail.com>
2015-04-12 22:40:16 -0400

    Profile Page Enhancements
    Improved the look and feel of the Profile Page.

--- ProfilePage.php
+++ ProfilePage.php
@@ -19,10 +19,6 @@
     <!-- jQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 
-    <!-- Start AngualrJS -->
-    <script src="javascripts/angular.min.js"></script>
-    <script src="javascripts/angular-touch.js"></script>
-
     <!-- Start Styles -->
     <link href="bootstrap-3.3.2-dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="bootstrap-3.3.2-dist/css/bootstrap-theme.min.css" rel="stylesheet" />
@@ -32,7 +28,7 @@
 
     <!--Start Java-->
     <script src="bootstrap-3.3.2-dist/js/bootstrap.min.js"></script>
-    <script src="bootstrap-3.3.2-dist/js/npm.js"></script>
+	<script src="javascripts/profileScript.js"></script>
 
     <!--Do Not REMOVE!-->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
@@ -40,23 +36,30 @@
 </head>
 
 
-<body id="Body">
+<body>
 	<?php if (login_check($mysqli_select) == true) :?>
 		<!-- User is logged in -- display profile page -->
 		<div class="container-fluid">
-				<div class="row">
-					<div id="Header" class="col-xs-12">
-						<h1>World Tree Circle <img src="images/WTC.gif" alt="World Tree Circle" style="width:50px;height:50px" /> </h1>
+				<div class="row Header">
+					<div class="col-xs-12">
+						<h1>World Tree Circle <img src="images/WTC.gif" alt="World Tree Circle" style="width:50px;height:50px" /></h1>
 
-						<div class="pull-right inline" height="100%">
+						<div class="pull-right logout">
 							<a href="includes/logout">Logout</a>
 						</div>
 					</div>
 				</div>
-				<div class="row">
+				<div id="navbar" class="row">
+					<div class="col-xs-12 hidden-sm">
+						<a id="btnLeftNav" class="btn btn-success pull-left">Profile <span id="leftGlyphicon" class="glyphicon glyphicon-chevron-right"></span></a>
+						<a id="btnRightNav" class="btn btn-success pull-right"><span id="rightGlyphicon" class="glyphicon glyphicon-chevron-left"></span> Message Center</a>
+					</div>
+				</div> 
+				<div class="row content">
 					<div id="LeftColumn" class="hidden-xs col-sm-2">
 						<div id="ProfilePicPanel" class="ProfilePicPanel">
 							<img style="width:100%;height:100%" alt="Paws" src="images/Paws%20(Pic%20Example).jpeg" />
+							<figcaption><?php echo $_SESSION['username'] ?></figcaption>
 						</div>
 						<br />
 						<div id="ProfileBio" class="ProfileBio">
---
+++ javascripts/profileScript.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    $('#btnLeftNav').click(function () {
+        if ($('#leftGlyphicon').hasClass('glyphicon-chevron-right')) {
+            $('#LeftColumn').removeClass('hidden-xs').addClass('col-xs-12');
+            $('#MainFeed').removeClass('col-xs-12').addClass('hidden-xs');
+            $('#leftGlyphicon').removeClass('glyphicon-chevron-right').addClass('glyphicon-chevron-left');
+            if ($('#rightGlyphicon').hasClass('glyphicon-chevron-right')) {
+                $('#MessageCenterPanel').removeClass('col-xs-12').addClass('hidden-xs');
+                $('#rightGlyphicon').removeClass('glyphicon-chevron-right').addClass('glyphicon-chevron-left');
+            }
+        }
+        else if ($('#leftGlyphicon').hasClass('glyphicon-chevron-left')) {
+            $('#LeftColumn').removeClass('col-xs-12').addClass('hidden-xs');
+            $('#MainFeed').removeClass('hidden-xs').addClass('col-xs-12');
+            $('#leftGlyphicon').removeClass('glyphicon-chevron-left').addClass('glyphicon-chevron-right');
+        }
+    });
+
+    $('#btnRightNav').click(function () {
+        if ($('#rightGlyphicon').hasClass('glyphicon-chevron-left')) {
+            $('#MessageCenterPanel').removeClass('hidden-xs').addClass('col-xs-12');
+            $('#MainFeed').removeClass('col-xs-12').addClass('hidden-xs');
+            $('#rightGlyphicon').removeClass('glyphicon-chevron-left').addClass('glyphicon-chevron-right');
+            if ($('#leftGlyphicon').hasClass('glyphicon-chevron-left')) {
+                $('#LeftColumn').removeClass('col-xs-12').addClass('hidden-xs');
+                $('#leftGlyphicon').removeClass('glyphicon-chevron-left').addClass('glyphicon-chevron-right');
+            }
+        }
+        else if ($('#rightGlyphicon').hasClass('glyphicon-chevron-right')) {
+            $('#MessageCenterPanel').removeClass('col-xs-12').addClass('hidden-xs');
+            $('#MainFeed').removeClass('hidden-xs').addClass('col-xs-12');
+            $('#rightGlyphicon').removeClass('glyphicon-chevron-right').addClass('glyphicon-chevron-left');
+        }
+    });
+});
